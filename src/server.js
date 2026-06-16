2c0bde35a1b189712fca339f74b08f4b28cc6df0
ASHOKJAAT12 <ajat39963@gmail.com>
2026-06-16 18:47:36 +0530

    mongodb connection successfull.

--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
+import dotenv from 'dotenv';
 import { app } from "./app.js";
+import connectionDB from './db/index.js';
+
+dotenv.config({
+    path: './.env'
+})
+const PORT = process.env.PORT || 4000;
+connectionDB()
+.then(()=>{
+    app.listen(PORT,(req, res)=>{
+        console.log(`server is listen on port ${PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("mongodb connection faild.");
+})
